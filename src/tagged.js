a75ff2fd4e985d3be05094610ee28523ae298d05
Carlos Eduardo <cpwaldow@gmail.com>
2021-12-22 13:00:19 -0300

    fix lint problems

--- src/tagged.js
+++ src/tagged.js
@@ -5,13 +5,13 @@ const something = 'Terra da garoa';
 const otherthing = 'Farialimers';
 
 function green(template, ...values) {
-  return template.reduce((acc, part, i) => {
-    return `
+  return template.reduce(
+    (acc, part, i) => `
       ${acc}
       <span class="green">${values[i - 1].toUpperCase()}</span>
       ${part}
-    `;
-  });
+    `,
+  );
 }
 
 const brazil = green`Eu moro em ${city} a cidade da ${something} e ${otherthing}!`;
