4d99aacb11dc6754aa9a12ba058fcc583ed0e6e2
Carlos Eduardo <cpwaldow@gmail.com>
2021-12-26 16:13:54 -0300

    spread operators

---
+++ src/spread_operator.js
@@ -0,0 +1,28 @@
+const front = ['react', 'vue', 'angular'];
+const back = ['python', 'ruby', 'nodejs'];
+
+/* console.log([...'will']); // ['w', 'i', 'l', 'l']
+console.log(...front); // react vue angular */
+
+// jeito antigo de concatenar
+// let fullstack = front.concat(back);
+
+// ES6
+
+const fullstack = [...front, ...back]; // ['react', 'vue', 'angular', 'python', 'ruby', 'nodejs']
+console.log(fullstack);
+console.log('');
+
+// spread operators dentro de função
+const makeSandwich = (bread, cheese, sauce) =>
+  `Your sandwish with  ${bread} bread, ${cheese} cheese and ${sauce} is done!`;
+
+const igredientes = ['white', 'cheddar', 'barbecue'];
+
+console.log(makeSandwich(...igredientes));
+
+const sum = (x, y, z) => x + y + z;
+
+const arrNumber = [10, 5, 19];
+
+console.log(sum(...arrNumber));
