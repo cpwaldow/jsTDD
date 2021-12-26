6a1fb1c4046d5d0bde6cda09b30a601472e52d2f
Carlos Eduardo <cpwaldow@gmail.com>
2021-12-26 09:04:38 -0300

    destructing

---
+++ src/destructing.js
@@ -0,0 +1,49 @@
+const data = {
+  name: 'Carlos',
+  surname: 'Eduardo',
+  age: 24,
+  social: {
+    twitter: '@cewaldow',
+    instagram: '/cpwaldow',
+  },
+};
+
+// utlizando destructing
+
+const { name, surname } = data;
+const { twitter, instagram } = data.social;
+
+console.log(name);
+console.log(surname);
+console.log(twitter);
+console.log(instagram);
+
+// mudando o nome da variável
+
+const { instagram: ig } = data.social;
+
+console.log(ig);
+
+// adicionando um valor default para uma variável caso não tenha no objeto
+
+// const { city } = data; // undefined
+
+const { city = 'São Paulo' } = data;
+
+console.log(city);
+console.log('');
+
+// redeclarar um valor de um item do objeto por um valor default
+
+// const { name = 'André' } = data; Identifier 'name' has already been declared
+
+// destructing em array
+
+const arr = ['Ivan', 'Gabriel', 25, 'Piraporinha'];
+
+const [nome, sobrenome, idade, cidade] = arr;
+
+console.log(nome);
+console.log(sobrenome);
+console.log(idade);
+console.log(cidade);
